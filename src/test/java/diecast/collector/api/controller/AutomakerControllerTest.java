1f42091fa66552edcd7abdc330e92e52d347956e
Leonardo Barbieri <leonardobdn@gmail.com>
2019-10-14 11:59:28 -0300

    Refactor test to create new automaker

--- src/test/java/diecast/collector/api/controller/AutomakerControllerTest.java
+++ src/test/java/diecast/collector/api/controller/AutomakerControllerTest.java
@@ -3,12 +3,14 @@ package diecast.collector.api.controller;
 import diecast.collector.api.client.AutomakerTestClient;
 import diecast.collector.api.domain.Automaker;
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @MicronautTest
@@ -23,19 +25,29 @@ public class AutomakerControllerTest {
         client = server.getApplicationContext().createBean(AutomakerTestClient.class, server.getURL());
     }
 
+    @AfterAll
+    public static void cleanUp() {
+        if (server != null) {
+            server.stop();
+        }
+    }
+
     @Test
     void testIsRunning() {
         assertTrue(server.isRunning());
     }
 
     @Test
-    public void testCreateAutomaker() {
+    public void testCreateAutomakerWhenBodyIsValid() {
         var automaker = new Automaker();
         automaker.setName("Honda");
         automaker.setCountry("Japan");
-        var response = client.save(automaker);
-        assertNotNull(response.body());
-        assertNotNull(response.body().getId());
+        var response = client.create(automaker);
+        assertThat(response.code()).isEqualTo(HttpStatus.CREATED.getCode());
+        var body = response.body();
+        assertThat(body).isNotNull();
+        assertThat(body.getId()).isNotNull();
+        assertThat(body.getName()).isEqualTo("Honda");
+        assertThat(body.getCountry()).isEqualTo("Japan");
     }
-
 }
