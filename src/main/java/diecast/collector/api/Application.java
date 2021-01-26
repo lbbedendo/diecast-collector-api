cfe62867fbb7c053fea1e32d5a3fa51640d8a76e
Leonardo Barbieri Bedendo <leonardobdn@gmail.com>
2021-01-26 10:25:30 -0300

    Adiciona suporte ao OpenApi/Swagger durante a fase de compilação

--- src/main/java/diecast/collector/api/Application.java
+++ src/main/java/diecast/collector/api/Application.java
@@ -1,7 +1,15 @@
 package diecast.collector.api;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Diecast Collector API",
+                version = "0.0"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
